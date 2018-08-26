7a60805a733d68570ee0b550804a72798fed9c84
ptiringo <ptiringo@gmail.com>
2018-08-26 09:57:32 +0900

    Use basic Vue.js functionality

--- app/main.js
+++ app/main.js
@@ -1,3 +1,28 @@
 var app = new Vue({
-  el: "#app"
+  el: "#app",
+  data: {
+    message: {
+      title: "英単語記録帳"
+    },
+    words: [
+      {
+        word: "word",
+        meaning: "単語",
+        level: 1,
+        registeredAt: "2018-08-26T00:30:00Z"
+      },
+      {
+        word: "meaning",
+        meaning: "意味",
+        level: 1,
+        registeredAt: "2018-08-26T00:40:00Z"
+      },
+      {
+        word: "log",
+        meaning: "記録",
+        level: 3,
+        registeredAt: "2018-08-27T00:40:00Z"
+      }
+    ]
+  }
 });
