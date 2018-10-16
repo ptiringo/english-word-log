a9ca866297be474e850feed73a8c4fff69e597c1
ptiringo <ptiringo@gmail.com>
2018-10-16 23:07:23 +0900

    Install vue-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+        use: ["vue-style-loader", "style-loader", "css-loader"]
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
       }
     ]
   }
